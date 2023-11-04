36db89da29bdf1de5927b946eb246986357038d1
2709patri <128839994+2709patri@users.noreply.github.com>
2023-11-04 08:32:29 -0300

    funciona ahora

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
             basico: 500,
             intermedio: 1000,
             premium: 1500,
+        
         };
     
         tipoSeguroSelect.addEventListener("change", function () {
@@ -23,5 +24,20 @@
             document.getElementById("mensaje").style.display = "block";
             document.getElementById("salir").style.display = "block";
         });
+        
+        const enviarSolicitudButton = document.getElementById("enviarSolicitud");
 
-    })
+    enviarSolicitudButton.addEventListener("click", function (e) {
+        e.preventDefault(); // Evita el envío del formulario si estás utilizando un formulario
+
+        // Redirige a la página de confirmación
+        window.location.href = "confirmacion.html";
+    });
+
+        
+
+      
+       });
+      
+
+    
